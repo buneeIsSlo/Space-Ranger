a920a3e71d2f4a8d7080edefe1be9597826601c5
bunee <buneeisslo@gmail.com>
2022-12-28 23:09:06 +0530

    Test: player sprite movements and animations

--- src/js/loadResources.js
+++ src/js/loadResources.js
@@ -1,17 +1,38 @@
 export const loadResources = () => {
-    loadSprite("background", "../assets/sprites/back.png");
-    loadSprite("foreground", "../assets/sprites/foreground.png");
+    loadSprite("background", "../assets/sprites/pinkBack.png");
 
-    loadSprite("ranger", "../assets/sprites/space-ranger-idle.png", {
-        sliceX: 4,
-        sliceY: 0,
+    loadSprite("tranger", "../assets/sprites/ranger64.png", {
+        sliceX: 27,
         anims: {
             idle: {
                 from: 0,
-                to: 3,
+                to: 4,
                 speed: 5,
                 loop: true,
             },
+            run: {
+                from: 5,
+                to: 14,
+                speed: 15,
+                loop: true,
+            },
+            jump: {
+                from: 15,
+                to: 20,
+                speed: 8,
+            },
+            die: {
+                from: 21,
+                to: 24,
+                speed: 8,
+            },
+            shoot: {
+                from: 25,
+                to: 26,
+                speed: 5
+            }
         },
-    })
+    });
+
+    loadSprite("floor", "../assets/sprites/floor.png");
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,20 @@ import kaboom from "kaboom";
 
 console.log("ohaider");
 
-const width = window.innerWidth;
-const height = window.innerHeight;
 
 kaboom({
-    width: width,
-    height: height,
+    width: 400,
+    height: 300,
     canvas: document.getElementById("game"),
+    scale: 2,
 });
 
+const PLAYER_SPEED = 320;
+
 import { loadResources } from "./loadResources";
+import { LEVEL } from "./levels";
 
 loadResources();
-
 
 scene("main", () => {
     layers([
@@ -24,31 +25,101 @@ scene("main", () => {
         "foreground",
     ], "game");
 
+    gravity(2400);
 
-    add([
-        text("Ohaider"),
-        pos(0, 0),
-    ]);
-
-
-    add([
-        sprite("background", { width: width, height: height }),
-        pos(0, 0),
-        layer("bg"),
-        fixed(),
-    ])
-
-
-    const ranger = add([
-        sprite("ranger"),
+    let tranger = add([
+        sprite("tranger"),
         layer("game"),
-        scale(2.2),
-        pos(center()),
+        pos(0, 0),
+        body(),
+        area({ width: 30 }),
     ])
-    ranger.play("idle");
+    console.log(tranger);
+    tranger.play("idle");
+    playerMovement(tranger);
+
+    add([
+        text("I eat hearts", { size: 24, font: "sink" }),
+        pos(center()),
+        origin("center"),
+        outline(6),
+        color([255, 0, 0])
+    ])
+
+    add([
+        sprite("background", { width: width(), height: height() }),
+        layer("bg"),
+        pos(0, 0),
+        fixed(true),
+    ])
+
+    const lvlConf = {
+        width: 16,
+        height: 16,
+        pos: (0, 0),
+
+        "=": () => [
+            sprite("floor"),
+            layer("game"),
+            area(),
+            solid(),
+            "floor",
+            pos(0, height() - 180),
+            origin("botleft"),
+        ],
+    }
+
+    addLevel(LEVEL, lvlConf);
 
 });
 
 go("main");
 
 
+function playerMovement(player) {
+
+    player.play("idle");
+
+    onKeyPress("down", () => {
+        player.play("die");
+    })
+
+    onKeyPress(["left", "right"], () => {
+        player.play("run", { loop: true });
+    })
+
+    onKeyDown("left", () => {
+        player.flipX(true);
+        player.move(-PLAYER_SPEED, 0);
+        console.log(player.curAnim());
+    })
+
+    onKeyDown("right", () => {
+        player.flipX(false);
+        player.move(PLAYER_SPEED, 0);
+        console.log(player.curAnim());
+
+    })
+
+    onKeyPress("space", () => {
+        if (!player.isGrounded()) return;
+
+        player.jump();
+        player.play("jump");
+    });
+
+    player.onUpdate(() => {
+        camPos(player.pos)
+    })
+
+    onKeyRelease(['left', 'right', 'down', 'up'], () => {
+        if (
+            !isKeyDown("left")
+            && !isKeyDown("right")
+            && !isKeyDown("up")
+            && !isKeyDown("down")
+        ) {
+            player.play("idle")
+        }
+    });
+}
