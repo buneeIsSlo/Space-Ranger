7442a15fbdf317c26101623cb2a7949e79018a52
bunee <buneeisslo@gmail.com>
2023-01-31 17:32:37 +0530

    Add: base to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./src",
+    base: "/space-ranger/",
     build: {
         assetsDir: "",
         outDir: "../dist",
